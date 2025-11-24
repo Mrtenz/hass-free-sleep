9f4395c543640f5280444c7385ec75ff4b01d46c
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-11-25 00:54:19 +0100

    fix: Add error handling to coordinator update (#17)
    This adds some error handling around the data coordinator update method,
    to raise an `UpdateFailed` error when fetching the data fails.

--- custom_components/free_sleep/coordinator.py
+++ custom_components/free_sleep/coordinator.py
@@ -8,11 +8,16 @@ from datetime import timedelta
 from logging import Logger
 from typing import Any, TypedDict
 
+from aiohttp import ClientError
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from homeassistant.helpers.update_coordinator import (
+  DataUpdateCoordinator,
+  UpdateFailed,
+)
 
 from .api import FreeSleepAPI
 from .constants import PodSide
+from .logger import log
 
 
 class PodState(TypedDict):
@@ -59,7 +64,26 @@ class FreeSleepCoordinator(DataUpdateCoordinator[PodState]):
       self.api.fetch_vitals('right'),
     ]
 
-    status, settings, vitals_left, vitals_right = await gather(*requests)
+    try:
+      status, settings, vitals_left, vitals_right = await gather(*requests)
+    except TimeoutError as error:
+      log.error(
+        f'Timeout while fetching data from device at "{self.api.host}".'
+      )
+      raise UpdateFailed from error
+    except ClientError as error:
+      log.error(
+        f'Client error while fetching data from device at "{self.api.host}":'
+        f'{error}'
+      )
+      raise UpdateFailed from error
+    except Exception as error:
+      log.error(
+        'Unexpected error while fetching data from device at'
+        f'"{self.api.host}": {error}'
+      )
+      raise UpdateFailed from error
+
     return PodState(
       status=status,
       settings=settings,
